46836ce72002b7b5fb443d9df9be6a9c63b5aa8a
curtis-clement <curtis.clement.nl@gmail.com>
2020-04-24 11:34:45 +0200

    minor changes

--- routers/users.js
+++ routers/users.js
@@ -1,10 +1,13 @@
 const {Router} = require('express');
 const User = require('../models').user;
+const Team = require('../models').team;
 
 const router = new Router();
 
 router.get('/', async(request, response, next) => {
-  const users = await User.findAll();
+  const users = await User.findAll({
+    include: [Team]
+  });
   response.status(200).send({message: 'success', users})
 });
 
